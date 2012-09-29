8df9fbdefcfa60111f298f7419774eca7cd1b950
eric <elesiuta@gmail.com>
2012-09-29 11:32:59 -0400

    code cleanup

--- ggc.py
+++ ggc.py
@@ -61,72 +61,37 @@ def primes(e):
     return primelist
 
 """
+Functions that make my list of primes and coprimes
+"""
+##good, need to use something other than python to improve further
+def betterlist(n, primes):
+    ##makes the set of coprimes that can be used to sum to any number multiple of n, requires list of primes (size depends on how much to test)
+    maxprime = primes[-1]
+    p = copy.copy(primes)
+    for x in pf(n,p):
+        if (primes.count(x)==1):
+            primes.remove(x)
+    coprimes = []
+    temp = []
+    for x in primes:
+        if x%n == 1:
+            coprimes.append(x)
+        else:
+            temp.append(x)
+    for x in range(n+1,maxprime,n):
+        has = True
+        for y in pf(x,p):
+            if temp.count(y) == 0:
+                has = False
+        if has == True:
+            coprimes.append(x)
+            for y in pf(x,p):
+                temp.remove(y)
+    return coprimes
+
+"""
 Functions that check which numbers can be written as a sum from my list
 """
-##depreciated
-def checksum(e, n, s):
-    ##check if e can be written as the sum of n numbers from set s
-    ##VERY INEFFICENT
-    for x in set(itertools.combinations_with_replacement(s,n)):
-        if (sum(x) == e):
-            return x
-    return False
-
-##depreciated
-def bettersum(n, s):
-    ##creates lis of e that can be written as the sum of n numbers from set s
-    output = []
-    for x in set(itertools.combinations_with_replacement(s,n)):
-        output.append(sum(x))
-    return list(sorted(collections.Counter(output)))
-
-##depreciated
-def maxsum(n, l):
-    ##finds the real max number from bettersum (since there are some stragglers higher up)
-    for i in range(0,len(l)):
-        if (l[i] != n * (i+1)):
-            return n*i
-    return l[-1]
-
-##depreciated
-def fastersum(n, s, upper):
-    ##creates lis of e that can be written as the sum of n numbers from set s
-    ##stores it a bit differently(hash) for efficiency, has upper limit, need to choose one big enough
-    output = [0]*upper
-    start = timeit.time.time()
-    for x in set(itertools.combinations_with_replacement(s,n)):
-        try:
-            output[int(sum(x)/n)] += 1
-        except:
-            pass
-    print (timeit.time.time() - start)
-    for i in range(1,len(output)):
-        if output[i] == 0:
-            return (i-1)*n
-    return (i*n)
-
-##just bad
-##Costs lots of ram, isn't that fast either
-def sumn(x):
-    return (int(sum(x)/len(x)))
-def mfastersum(n, s, upper):
-    ##creates lis of e that can be written as the sum of n numbers from set s
-    ##stores it a bit differently(hash) for efficiency, has upper limit, need to choose one big enough
-    output = [0]*upper
-    start = timeit.time.time()
-    pool = Pool(PROCESSES)
-    for x in pool.map(sumn,itertools.combinations_with_replacement(s,n),1000):
-        try:
-            output[x] += 1
-        except:
-            pass
-    print (timeit.time.time() - start)
-    return output
-    for i in range(1,len(output)):
-        if output[i] == 0:
-            return (i-1)*n
-    return (i*n)
-
 ##SINGLE PROCESS
 ##AHHHHHH THIS LOOKS DISGUSTING BUT I DONT KNOW HOW ELSE TO DO THIS
 ##currently the best version
@@ -161,194 +126,10 @@ def smallersum(n, s, upper, q):
             return (i-1)*n
     return (i*n)
 
-##MULTI PROCESS - I didn't really know what I was doing
-def mcombinations(cd, md, sum, i, l):
-    ##returns sum of combinations
-    ##current depth, max depth, sum, index, list
-    output = []
-    if cd < md:
-        for x in range(i, len(l)):
-            output += mcombinations(cd+1,md,sum+l[x],x,l)
-    else:
-        for x in range(i, len(l)):
-            output.append(int((sum+l[x])/md))
-    return output
-
-def mcombinations_wrapper(args):
-    ##for parallel use
-    return mcombinations(args[0],args[1],args[2],args[3],args[4])
-
-def multisum(n, s, upper):
-    ##uses parallel processes (map)
-    ##creates lis of e that can be written as the sum of n numbers from set s
-    ##uses my own function for combinations so they don't all stay stored in memory
-    global globlist
-    globlist = [0]*upper
-    pool = Pool(PROCESSES)
-    ##try sending in globlist as a parameter here and pass by reference (don't know how python does this)
-    for i in pool.map(mcombinations_wrapper,[(2,n,s[x],x,s) for x in range(0,len(s))],10000):
-        for x in i:
-            try:
-                globlist[x] += 1
-            except:
-                pass
-    ##return globlist
-    for i in range(1,len(globlist)):
-        if globlist[i] == 0:
-            return (i-1)*n
-    return (i*n)
-
-##Now I actually might know something - passing globlist by reference (I hope, python seems to do whatever it feels like, maybe try ctypes)
-##ValueError: ctypes objects containing pointers cannot be pickled
-##It seems python is determined to make this impossible, as shown by that error (works with single process)
-def refcombinations(cd, md, sum, i, l, reflist):
-    ##returns sum of combinations
-    ##current depth, max depth, sum, index, list
-    for x in range(i, len(l)):
-        if cd < md:
-            combinations(cd+1,md,sum+l[x],x,l)
-        else:
-            try:
-                reflist.contents[int((sum+l[x])/md)] += 1
-            except:
-                pass
-
-def refcombinations_wrapper(args):
-    ##for parallel use
-    refcombinations(args[0],args[1],args[2],args[3],args[4],args[5])
-
-def refsum(n, s, upper):
-    ##creates lis of e that can be written as the sum of n numbers from set s
-    ##uses my own function for combinations so they don't all stay stored in memory
-    newlist = ctypes.ARRAY(ctypes.c_int,upper)(*[0]*upper)
-    reflist = ctypes.pointer(newlist)
-    pool = Pool(PROCESSES)
-    pool.map(refcombinations_wrapper,[(2,n,s[x],x,s,reflist) for x in range(0,len(s))],1000)
-    print(reflist)
-    for i in range(1,len(reflist.contents)):
-        if reflist.contents[i] == 0:
-            return (i-1)*n
-    return (i*n)
-
-##don't know the advantages/workings of async yet
-def asyncsum(n, s, upper):
-    ##uses parallel processes (async)
-    ##creates lis of e that can be written as the sum of n numbers from set s
-    ##uses my own function for combinations so they don't all stay stored in memory
-    global globlist
-    globlist = [0]*upper
-    pool = Pool(PROCESSES)
-    for x in range(0,len(s)):
-        pool.apply_async(func = combinations, args = (2,n,s[x],x,s))
-    for i in range(1,len(globlist)):
-        if globlist[i] == 0:
-            return (i-1)*n
-    return (i*n)
-
-"""
-Functions that make my list of primes and coprimes
-"""
-##depreciated, logic error but may have other significance
-def makelist(n,primes):
-    ##BUG ON n = 7, should choose 2^3 over 2*11, keep code just in case, I like what I did and may use somewhere else
-    ##makes the set of coprimes that can be used to sum to any number multiple of n, requires list of primes (size depends on how much to test)
-    maxprime = primes[-1]
-    for x in pf(n):
-        if (primes.count(x)==1):
-            primes.remove(x)
-    coprimes = []
-    temp = []
-    for x in primes:
-        if x%n == 1:
-            coprimes.append(x)
-        else:
-            temp.append(x)
-    while temp != []:
-        ln = temp
-        lp = [0]*len(temp)
-        lp[0] = 2
-        while(resolve(ln,lp)%n != 1):
-            try:
-                ##I think I fixed it, should work
-                if (ln[0]**lp[0] < ln[1]):
-                    lp[0] += 1
-                elif(ln[0]**lp[0] > ln[1]):
-                    lp[0] = 1
-                    lp[1] += 1
-                    ##like counting in a number system where each place is a different base (LS# != 0, maybe not?) (this was my flaw in logic for n=7)
-                    for x in range(1,len(lp)-1):
-                        if((ln[x]**lp[x] > ln[x+1])):
-                            lp[x] = 0
-                            lp[x+1] += 1
-            except:
-                lp[0] += 1
-        ##little bit of cleanup, no point working with sparse numbers about what we're testing
-        coprimes.append(resolve(ln,lp))
-        if (coprimes[-1] > maxprime):
-            return coprimes[0:-1]
-        temp = clean(ln,lp)
-    return coprimes
-
-##depreciated
-def resolve(ln,lp):
-    ##list of numbers, list of powers
-    ##eg. ln = [2,3,5,7], lp = [2,1,0,0], is 2^2*3^1*5^0*7^0
-    ##used by makelist
-    x = 1
-    for i in range(0,len(ln)):
-        x *= ln[i]**lp[i]
-    return x
-
-##depreciated
-def clean(ln,lp):
-    ##used by makelist
-    ##takes out any primes that are used so all the numbers in the final list are coprime
-    lc = []+ln
-    for i in range(0,len(ln)):
-        if lp[i] != 0:
-            lc.remove(ln[i])
-    return lc
-
-##good, need to use something other than python to improve further
-def betterlist(n, primes):
-    ##makes the set of coprimes that can be used to sum to any number multiple of n, requires list of primes (size depends on how much to test)
-    maxprime = primes[-1]
-    p = copy.copy(primes)
-    for x in pf(n,p):
-        if (primes.count(x)==1):
-            primes.remove(x)
-    coprimes = []
-    temp = []
-    for x in primes:
-        if x%n == 1:
-            coprimes.append(x)
-        else:
-            temp.append(x)
-    for x in range(n+1,maxprime,n):
-        has = True
-        for y in pf(x,p):
-            if temp.count(y) == 0:
-                has = False
-        if has == True:
-            coprimes.append(x)
-            for y in pf(x,p):
-                temp.remove(y)
-    return coprimes
 
 """
 Functions that combine everything for a quick test of number n
 """
-##depreciated
-def oldquicktest(n,upper):
-    s = betterlist(n,primes(upper))
-    a = bettersum(n,s)
-    return maxsum(n,a)
-
-##depreciated
-def quicktest(n,upper):
-    s = betterlist(n,primes(upper))
-    return fastersum(n,s,upper*5)
-
 def smalltest(n,upper,q):
     s = betterlist(n,primes(upper))
     return smallersum(n,s,upper*5,q)
@@ -357,21 +138,6 @@ def smalltest_wrapper(arg):
     output = smalltest(arg[0],arg[1],arg[2])
     arg[2].put([arg[0],output])
     return output
-
-##bad
-def multitest(n,upper):
-    s = betterlist(n,primes(upper))
-    return multisum(n,s,upper*5)
-
-##bad
-def mfastertest(n,upper):
-    s = betterlist(n,primes(upper))
-    return mfastersum(n,s,upper*5)
-
-##Python, Y U NO pass by reference?
-def reftest(n,upper):
-    s = betterlist(n,primes(upper))
-    return refsum(n,s,upper*5)
 
 """
 Test a bunch of n
@@ -420,7 +186,7 @@ def betterbatch(start, stop, size):
             awake += 1
         while not q.empty():
             print(q.get())
-        time.sleep(5)
+        time.sleep(60)
     b = a.get()
     pool.join()
     ##for x in b:
